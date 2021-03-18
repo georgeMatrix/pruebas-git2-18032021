c3b2bdf9eb1db1fca75d1de2508e0293f25ac3fb
GeorgeX <jorge@gmail.com>
2021-03-18 16:33:13 -0600

    ClienteService

---
+++ src/main/java/com/formatoweb/pruebasgit218032021/service/ClienteService.java
@@ -0,0 +1,9 @@
+package com.formatoweb.pruebasgit218032021.service;
+
+import com.formatoweb.pruebasgit218032021.entity.Cliente;
+
+import java.util.List;
+
+public interface ClienteService {
+    List<Cliente> getClientes();
+}
