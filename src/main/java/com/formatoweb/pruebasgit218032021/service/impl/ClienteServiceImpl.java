8f27e02a88f59e6659dca2468769a4c168e5d71b
GeorgeX <jorge@gmail.com>
2021-03-18 17:24:56 -0600

    ClienteController, ClienteServiceImpl

--- src/main/java/com/formatoweb/pruebasgit218032021/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/formatoweb/pruebasgit218032021/service/impl/ClienteServiceImpl.java
@@ -1,28 +1,33 @@
 package com.formatoweb.pruebasgit218032021.service.impl;
 
 import com.formatoweb.pruebasgit218032021.entity.Cliente;
+import com.formatoweb.pruebasgit218032021.repository.ClienteRepository;
 import com.formatoweb.pruebasgit218032021.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class ClienteServiceImpl implements ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepository;
     @Override
     public List<Cliente> getClientes() {
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente saveClientes(Cliente cliente) {
-        return null;
+        return clienteRepository.save(cliente);
     }
 
     @Override
     public Cliente clienteById(Long id) {
-        return null;
+        return clienteRepository.findById(id).orElse(null);
     }
 
     @Override
     public void deleteCliente(Long id) {
-
+        clienteRepository.deleteById(id);
     }
 }
