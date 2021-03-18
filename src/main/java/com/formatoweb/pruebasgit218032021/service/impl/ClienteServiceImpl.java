d24974beca30d2a23e934e86069912b75a2027a5
GeorgeX <jorge@gmail.com>
2021-03-18 17:26:07 -0600

    Revert "ClienteController, ClienteServiceImpl"
    This reverts commit 8f27e02a

--- src/main/java/com/formatoweb/pruebasgit218032021/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/formatoweb/pruebasgit218032021/service/impl/ClienteServiceImpl.java
@@ -1,33 +1,28 @@
 package com.formatoweb.pruebasgit218032021.service.impl;
 
 import com.formatoweb.pruebasgit218032021.entity.Cliente;
-import com.formatoweb.pruebasgit218032021.repository.ClienteRepository;
 import com.formatoweb.pruebasgit218032021.service.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
+
 public class ClienteServiceImpl implements ClienteService {
-    @Autowired
-    private ClienteRepository clienteRepository;
     @Override
     public List<Cliente> getClientes() {
-        return clienteRepository.findAll();
+        return null;
     }
 
     @Override
     public Cliente saveClientes(Cliente cliente) {
-        return clienteRepository.save(cliente);
+        return null;
     }
 
     @Override
     public Cliente clienteById(Long id) {
-        return clienteRepository.findById(id).orElse(null);
+        return null;
     }
 
     @Override
     public void deleteCliente(Long id) {
-        clienteRepository.deleteById(id);
+
     }
 }
