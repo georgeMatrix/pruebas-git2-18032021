e087d557cf2bcf7731d89d627397f9e48727203e
GeorgeX <jorge@gmail.com>
2021-03-18 17:36:29 -0600

    ClienteController get

--- src/main/java/com/formatoweb/pruebasgit218032021/controllers/ClienteController.java
+++ src/main/java/com/formatoweb/pruebasgit218032021/controllers/ClienteController.java
@@ -1,9 +1,21 @@
 package com.formatoweb.pruebasgit218032021.controllers;
 
+import com.formatoweb.pruebasgit218032021.entity.Cliente;
+import com.formatoweb.pruebasgit218032021.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+    @GetMapping("/cliente")
+    public List<Cliente> getCliente(){
+        return clienteService.getClientes();
+    }
 }
