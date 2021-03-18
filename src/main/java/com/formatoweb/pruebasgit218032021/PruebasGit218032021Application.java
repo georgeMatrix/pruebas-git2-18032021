e5cb9e4651d3b7074158175247d0eb6b71f70d9f
GeorgeX <jorge@gmail.com>
2021-03-18 15:30:28 -0600

    primera carga

---
+++ src/main/java/com/formatoweb/pruebasgit218032021/PruebasGit218032021Application.java
@@ -0,0 +1,13 @@
+package com.formatoweb.pruebasgit218032021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasGit218032021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebasGit218032021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/formatoweb/pruebasgit218032021/PruebasGit218032021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formatoweb.pruebasgit218032021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasGit218032021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
