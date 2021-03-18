dbd9950bc4733bfbe9e95c45d502084765b48286
GeorgeXVirtual <georgeXVirtual>
2021-03-18 17:35:56 -0600

    ClienteController

--- src/main/java/com/formatoweb/pruebasgit218032021/controllers/ClienteController.java
+++ src/main/java/com/formatoweb/pruebasgit218032021/controllers/ClienteController.java
@@ -1,9 +1,22 @@
 package com.formatoweb.pruebasgit218032021.controllers;
 
+import com.formatoweb.pruebasgit218032021.entity.Cliente;
+import com.formatoweb.pruebasgit218032021.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+    @PostMapping("/cliente")
+    public Cliente saveCliente(Cliente cliente){
+        return clienteService.saveClientes(cliente);
+    }
 }
