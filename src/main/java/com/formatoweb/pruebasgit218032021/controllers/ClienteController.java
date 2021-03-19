0ca5807f49efc3379fa30c767b35d6085e21cf1a
GeorgeX <jorge@gmail.com>
2021-03-18 18:23:14 -0600

    ClienteController getById ResponseEntity

--- src/main/java/com/formatoweb/pruebasgit218032021/controllers/ClienteController.java
+++ src/main/java/com/formatoweb/pruebasgit218032021/controllers/ClienteController.java
@@ -3,13 +3,18 @@ package com.formatoweb.pruebasgit218032021.controllers;
 import com.formatoweb.pruebasgit218032021.entity.Cliente;
 import com.formatoweb.pruebasgit218032021.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.formatoweb.pruebasgit218032021.entity.Cliente;
 import com.formatoweb.pruebasgit218032021.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -22,8 +27,19 @@ public class ClienteController {
     }
 
     @PostMapping("/cliente")
-    public Cliente saveCliente(Cliente cliente){
-        return clienteService.saveClientes(cliente);
+    public ResponseEntity<?> saveCliente(Cliente cliente){
+        Map<String, Object> response = new HashMap<>();
+        Cliente newCliente = new Cliente();
+        try{
+            newCliente = clienteService.saveClientes(cliente);
+        }catch (DataAccessException e){
+            response.put("mensaje", "Error al cargar dato en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("mensaje", "Tu dato ha sido cargado satisfactoriamente");
+        response.put("dato", newCliente);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("/cliente/{id}")
